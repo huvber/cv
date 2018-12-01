a6f72401cf2af4123d1acbdfec4c08a6bf6eb792
Andrea Mannarà <huvber@gmail.com>
2018-12-01 14:02:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Header 
+    :title="firstname +' '+ lastname"
+    avatar-url="/assets/avatar.png" />
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      firstname: '',
+      lastname: ''
+    }
+  },
+  mounted: function() {
+    this.firstname = 'Andrea'
+    this.lastname = 'Mannarà'
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import './styles/reset.scss';
+
+body {
+  max-width: 1024px;
+  border: 1px solid #333;
+  margin: 0 auto;
+}
+</style>
