a55897a8a3ce0bca0896aa82f3f86b6222a47bf5
Andrea Mannarà <huvber@gmail.com>
2018-12-01 14:02:19 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <section class="avatar">
+      <Avatar :src="avatarUrl" />
+    </section>
+    <section>
+      <h1>{{title}}</h1>
+    </section>
+  </header>
+</template>
+
+
+<script>
+//import SkillList from './SkillList';
+import Avatar from './Avatar';
+
+export default {
+  name: 'Header',
+  props: ['title', 'avatarUrl'],
+  components: {
+    Avatar
+  }
+}
+</script>
+
+<style lang="scss">
+.avatar {
+  width: 200px;
+  height: 200px;
+}
+</style>
