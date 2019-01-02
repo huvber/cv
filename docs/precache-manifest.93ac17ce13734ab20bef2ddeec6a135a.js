7bf620f1e113ee2b6a9f1457c50780e04362b7db
Andrea Mannarà <huvber@gmail.com>
2019-01-03 00:12:22 +0100

    add docs

---
+++ docs/precache-manifest.93ac17ce13734ab20bef2ddeec6a135a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a01420ae84b773d1b24a",
+    "url": "/css/app.99160278.css"
+  },
+  {
+    "revision": "a01420ae84b773d1b24a",
+    "url": "/js/app.521c90e1.js"
+  },
+  {
+    "revision": "62254544c24be0400d27",
+    "url": "/js/chunk-vendors.6503eba4.js"
+  },
+  {
+    "revision": "4a8de0f4a96007e7d0dc32f45df125a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/assets/avatar.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
